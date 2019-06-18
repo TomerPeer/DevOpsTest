d3a26c67d0e1807d33fd413d873b28ea98940b68
TomerPeer <tomerpeer2002@gmail.com>
2019-06-18 21:01:00 +0300

    test

---
+++ counter.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var visits = 0;
+
+http.createServer( function(request, response) {
+
+   console.log('New Connection');
+   /*console.log(request.method);
+   response.end();*/
+
+  switch(request.method){
+    case('POST'):
+        visits++;
+        console.log('POST');
+        break;
+    case('GET'):
+        console.log('GET');   
+        response.writeHead(200, { 'Content-Type':'text/plain' } );
+        response.write('Hello\n');
+        response.write('We have had '+visits+' visits');
+        break;
+    default:
+        break;
+  }
+  
+  response.end();
+}).listen(8080);
+
+console.log('Server Started..');
